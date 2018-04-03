073ffc39c1401d7f1c40691a4ddcac59aa5764ff
Rachel Miles <milesrachel9@gmail.com>
2018-04-03 11:36:58 -0600

    backend built

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -8,13 +8,28 @@ class Api::CoinsController < ApplicationController
   end
 
   def create
+    cmc_id = params[:coin].downcase
+    res = HTTPARTY.get('#{BASE_URL}#{cmc_id}')
+    if coin = Coin.create_by_cmc_id(res)
+      watched = WatchedCoin.find_or_create_by(
+        coin_id: coin.id,
+        user_id: current_user.id
+      )
+      watched.update(initial_price: coin.price) if watched.initial_price.nil?
+    else
+      render json: { errors: 'Coin Not Found' } , status: 422
   end
 
   def show
     render json: @coin 
   end
 
+  # PUT /api/coins/:id --- DOES NOT UPDATE. instead of creating a whole new controller I will use this unused one.
   def update
+    current_user
+      .watched_coins
+      .find_by(coin_id: @coin.id)
+      .destroy 
   end
 
   def destroy
